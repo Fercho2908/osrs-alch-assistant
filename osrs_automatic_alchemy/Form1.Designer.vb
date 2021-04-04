10293cb4779fd4dfbfd2b39aec777d5b24576f12
Fercho <fefc2908@gmail.com>
2021-04-04 15:08:55 -0400

    First commit, currently it can't be stopped once executed

---
+++ osrs_automatic_alchemy/Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.SteelBlue
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel1.Location = New System.Drawing.Point(199, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1, 142)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.SteelBlue
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel2.Location = New System.Drawing.Point(0, 141)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(199, 1)
+        Me.Panel2.TabIndex = 0
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.SteelBlue
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel3.Location = New System.Drawing.Point(0, 0)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(199, 1)
+        Me.Panel3.TabIndex = 0
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.SteelBlue
+        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel4.Location = New System.Drawing.Point(0, 1)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(1, 140)
+        Me.Panel4.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.BackColor = System.Drawing.Color.SteelBlue
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SteelBlue
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSkyBlue
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(12, 104)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(176, 30)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Ejecutar"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button3.BackgroundImage = Global.osrs_alch_assistant.My.Resources.Resources.Icono_Minimizar
+        Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button3.FlatAppearance.BorderSize = 0
+        Me.Button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.Button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Location = New System.Drawing.Point(140, 0)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(30, 30)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.BackgroundImage = Global.osrs_alch_assistant.My.Resources.Resources.Icono_cerrar_FN
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SteelBlue
+        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(170, 0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(30, 30)
+        Me.Button2.TabIndex = 2
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.DimGray
+        Me.TextBox1.Location = New System.Drawing.Point(12, 42)
+        Me.TextBox1.MaxLength = 7
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(176, 23)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "Repeticiones"
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.Color.SteelBlue
+        Me.Panel5.Location = New System.Drawing.Point(12, 65)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(176, 1)
+        Me.Panel5.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DimGray
+        Me.Label1.Location = New System.Drawing.Point(8, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 23)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Repeticiones"
+        Me.Label1.Visible = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Comic Sans MS", 9.75!)
+        Me.CheckBox1.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 76)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(108, 22)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "Valuable item"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(200, 142)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Alch assistant"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+End Class
---
+++ osrs_automatic_alchemy/Form1.vb
@@ -0,0 +1,235 @@
+﻿Imports System.Threading
+
+Public Class Form1
+    Dim hilo As Thread
+    Dim hilo_inicio As New ThreadStart(AddressOf label_animation1)
+    Dim num_random As New Random
+    Dim ex, ey As Integer
+    Dim Arrastre As Boolean
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        ex = e.X
+        ey = e.Y
+        Arrastre = True
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        Arrastre = False
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        If Arrastre Then
+            Location = PointToScreen(New Point(MousePosition.X - Location.X - ex, MousePosition.Y - Location.Y - ey))
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub TextBox1_Enter(sender As Object, e As EventArgs) Handles TextBox1.Enter
+        Panel5.BackColor = Color.LightSkyBlue
+
+        Dim ubicaion = New Point(8, 42)
+
+        If (Label1.Location = ubicaion And TextBox1.ForeColor = Color.DimGray) Then
+            Try
+                Label1.Visible = True
+
+                hilo_inicio = New ThreadStart(AddressOf label_animation1)
+                hilo = New Thread(hilo_inicio)
+                hilo.IsBackground = True
+                hilo.Start()
+
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            End Try
+        End If
+    End Sub
+
+    Private Sub TextBox1_Leave(sender As Object, e As EventArgs) Handles TextBox1.Leave
+        Panel5.BackColor = Color.SteelBlue
+
+        Dim ubicaion = New Point(8, 22)
+
+        If (Label1.Location = ubicaion And TextBox1.Text.Equals("")) Then
+            Try
+
+                'hilo_inicio = New ThreadStart(AddressOf label_animation2)
+                'hilo = New Thread(hilo_inicio)
+                'hilo.IsBackground = True
+                'hilo.Start()
+
+                Label1.ForeColor = Color.DimGray
+                Label1.Location = New Point(8, 42)
+                Label1.Font = New Font(Label1.Font.Name, 12)
+                Label1.Visible = False
+                TextBox1.ForeColor = Color.DimGray
+                TextBox1.Text = "Repeticiones"
+                TextBox1.Enabled = False
+                TextBox1.Enabled = True
+
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            End Try
+        End If
+    End Sub
+
+    Private Sub label_animation1()
+        Try
+            Form1.CheckForIllegalCrossThreadCalls = False
+
+            Label1.ForeColor = Color.White
+            TextBox1.ForeColor = Color.White
+            TextBox1.Text = ""
+
+            For i As Integer = 0 To 3
+
+                Label1.Location = New Point(Label1.Location.X, Label1.Location.Y - 5)
+
+                If (Label1.Location.Y = 32 Or Label1.Location.Y = 22) Then
+                    Label1.Font = New Font(Label1.Font.Name, Label1.Font.SizeInPoints - 1)
+                End If
+
+                Thread.Sleep(25)
+            Next
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    'Private Sub label_animation2()
+    '    Try
+    '        Form1.CheckForIllegalCrossThreadCalls = False
+
+    '        For i As Integer = 0 To 3
+
+    '            If (Label1.Location.Y = 32 Or Label1.Location.Y = 22) Then
+    '                Label1.Font = New Font(Label1.Font.Name, Label1.Font.SizeInPoints + 1)
+    '            End If
+
+    '            Label1.Location = New Point(Label1.Location.X, Label1.Location.Y + 5)
+
+    '            Thread.Sleep(25)
+    '        Next
+
+    '        Label1.ForeColor = Color.DimGray
+    '        Label1.Location = New Point(8, 42)
+    '        Label1.Font = New Font(Label1.Font.Name, 12)
+    '        Label1.Visible = False
+    '        TextBox1.ForeColor = Color.DimGray
+    '        TextBox1.Text = "Repeticiones"
+
+    '    Catch ex As Exception
+    '        MsgBox(ex.ToString)
+    '    End Try
+    'End Sub
+
+    Public Declare Sub mouse_event Lib "user32" Alias "mouse_event" (ByVal dwFlags As Long)
+    'LEFT BUTTON DOWN = &H2
+    'LEFT BUTTON UP = &H4
+    'RIGHT BUTTON DOWN = &H8
+    'RIGHT BUTTON UP = &H10
+    'MIDDLE BUTTON DOWN = &H20
+    'MIDDLE BUTTON UP = &H40
+
+    Private Sub solo_numeros(e As KeyPressEventArgs)
+        If (Char.IsNumber(e.KeyChar) Or Char.IsControl(e.KeyChar)) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        solo_numeros(e)
+    End Sub
+
+    Private Sub cast_alchemy(replays)
+        Try
+            If (Char.IsNumber(TextBox1.Text)) Then
+                replays = TextBox1.Text
+
+                WindowState = FormWindowState.Minimized
+
+                Dim mouse = New Point(num_random.Next(1277, 1284), num_random.Next(527, 536))
+
+                For i As Integer = 1 To replays
+
+                    If (Cursor.Position <> mouse) Then
+                        Cursor.Position = New Point(mouse)
+                    End If
+
+                    If (CheckBox1.Checked) Then
+
+                        mouse_event(&H2)
+                        mouse_event(&H4)
+                        Thread.Sleep(num_random.Next(300, 400))
+
+                        If (Cursor.Position <> mouse) Then
+                            Cursor.Position = New Point(mouse)
+                        End If
+
+                        mouse_event(&H2)
+                        mouse_event(&H4)
+                        Thread.Sleep(num_random.Next(1600, 1700))
+
+                        My.Computer.Keyboard.SendKeys(" ")
+                        Thread.Sleep(num_random.Next(1400, 1500))
+
+                        My.Computer.Keyboard.SendKeys("1")
+
+                    Else
+
+                        mouse_event(&H2)
+                        mouse_event(&H4)
+
+                        Thread.Sleep(num_random.Next(350, 480))
+
+                        If (Cursor.Position <> mouse) Then
+                            Cursor.Position = New Point(mouse)
+                        End If
+
+                        mouse_event(&H2)
+                        mouse_event(&H4)
+
+                    End If
+
+                    Thread.Sleep(num_random.Next(3000, 4000))
+
+                    If (i = replays) Then
+                        WindowState = FormWindowState.Normal
+                        MsgBox("Listo perro😎")
+                    End If
+                Next
+
+                TextBox1.Text = "Repeticiones"
+                TextBox1.ForeColor = Color.DimGray
+                Label1.ForeColor = Color.DimGray
+                Label1.Location = New Point(8, 42)
+                Label1.Font = New Font(Label1.Font.Name, 12)
+                Label1.Visible = False
+                CheckBox1.Checked = False
+                CheckBox1.Focus()
+                WindowState = FormWindowState.Minimized
+            Else
+                MsgBox("Debe ingresar el número de items para hacer alchemy")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cast_alchemy(TextBox1.Text)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        AcceptButton = Button1
+    End Sub
+End Class
